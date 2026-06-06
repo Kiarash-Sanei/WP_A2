0e18b9d593cf8358b4689aa36597f6c08a0a19b6
Kiarash Sanei <kiarashsanei5@gmail.com>
2026-06-06 13:58:04 +0330

    feat: Requests are now ok

--- components/request/UrlBar.tsx
+++ components/request/UrlBar.tsx
@@ -3,29 +3,55 @@
 import { METHODS } from "@/constants/methods";
 import { useTab } from "@/contexts/TabContext";
 import { useState } from "react";
+import { LoadingSpinner } from "../ui/LoadingSpinner";
 
 export function UrlBar() {
     const { tabs, activeTabId, updateTab } = useTab();
     const activeTab = tabs.find(tab => tab.id === activeTabId);
     const [error, setError] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     
     const isValidUrl = (url: string): boolean => {
         return url.startsWith("http://") || url.startsWith("https://");
     };
 
-    const handleSend = () => {
+    const handleSend = async () => {
         if (!activeTab?.url || !isValidUrl(activeTab.url)) {
             setError("Please enter a valid URL (http:// or https://)");
             return;
         }
         setError(null);
-        // actual request logic comes later
+        try {
+            setIsLoading(true);
+            
+            const headersObj = Object.fromEntries(
+            activeTab.headers
+                .filter(h => h.enabled && h.key)
+                .map(h => [h.key, h.value])
+            );
+
+            const response = await fetch(activeTab.url, {
+                method: activeTab.method,
+                headers: headersObj,
+                body: ["GET", "DELETE"].includes(activeTab.method) ? undefined : activeTab.body,
+            });
+
+            const body = await response.text();
+            updateTab(activeTabId, {
+                response: { status: response.status, body: body }
+            });
+        } catch (error) {
+            setError(error instanceof Error ? error.message : "An unexpected error occurred");
+        } finally {
+            setIsLoading(false);
+        }
     };
 
     return (
         <>
             {activeTab ?
                 <div className="flex items-center">
+                    <LoadingSpinner isLoading={isLoading}/>
                     <select 
                         value={activeTab?.method}
                         onChange={(value) => updateTab(activeTabId, { method: value.target.value })}
