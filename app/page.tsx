6e57007fd3a87f94cedf2deac366dc21c6d1e0f5
Kiarash Sanei <kiarashsanei5@gmail.com>
2026-06-09 11:34:01 +0330

    feat: Main page background is ok

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,19 @@ import { MainPanel } from "@/components/layout/MainPanel";
 import { SideBar } from "@/components/layout/SideBar";
 import { TabBar } from "@/components/layout/TabBar";
 import { TopBar } from "@/components/layout/TopBar";
+import { Box } from "@mui/material";
 
 export default function Home() {
   return (
-    <div className="flex h-screen">
+    <Box
+      sx={{ display: "flex", height: "100vh", bgcolor: "background.default" }}
+    >
       <SideBar />
-      <div className="flex flex-col flex-1">
+      <Box sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
         <TopBar />
         <TabBar />
         <MainPanel />
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 }
